c1e80bcc9d990f81bf21667aaf6ec1cb058aab8f
FabiClawZ <ffs2309.fs@gmail.com>
2025-08-23 20:28:41 +0200

    Kinda fix for 16x9 1p layout

--- src/graphics/pages/game-layout/layouts/Layout_16x9_1g1c.vue
+++ src/graphics/pages/game-layout/layouts/Layout_16x9_1g1c.vue
@@ -26,7 +26,7 @@
         <large-separator direction="vertical" />
         <div class="layout vertical">
             <div
-                class="game-capture layout-gap-bottom max-height"
+                class="game-capture layout-gap-bottom"
                 data-capture-index="0"
             />
             <div class="timer-wrapper bg-panel bg-timer blur-bg layout horizontal center-vertical center-horizontal">
